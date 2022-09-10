15a5e41532a27859ed7af0f45988ce0c246ed6a7
Muhammad Salman Shahid <salman.shahid@empglabs.com>
2022-09-10 11:26:05 +0500

    postgressql

--- database/migrations/2022_09_09_120505_create_clients_table.php
+++ database/migrations/2022_09_09_120505_create_clients_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('clients', function (Blueprint $table) {
+            
             $table->id();
             $table->string('name', 255);
             $table->string('unique_id', 255);
