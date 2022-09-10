742cb7fe0c334474b9a4401dbeb5825b296ff4ba
Muhammad Salman Shahid <salman.shahid@empglabs.com>
2022-09-10 11:08:04 +0500

    scripts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 255);
             $table->string('email', 255);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
