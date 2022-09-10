98f1d42674648b1124bbc473f86696d514996a71
Muhammad Salman Shahid <salman.shahid@empglabs.com>
2022-09-10 11:12:36 +0500

    scripts

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        return;
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
